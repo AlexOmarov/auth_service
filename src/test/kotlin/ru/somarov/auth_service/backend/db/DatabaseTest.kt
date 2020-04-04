90e29131aded6cd7ffb0a2ce5aa6a5f27623f43f
Alexandr Omarov <alexandr.omarov@sibit.tech>
2020-04-04 15:45:10 +0700

    Logback config

--- src/test/kotlin/ru/somarov/auth_service/backend/db/DatabaseTest.kt
+++ src/test/kotlin/ru/somarov/auth_service/backend/db/DatabaseTest.kt
@@ -1,9 +1,11 @@
 package ru.somarov.auth_service.backend.db
 
+import com.github.dockerjava.api.DockerClient
 import org.junit.Test
 import org.junit.jupiter.api.BeforeAll
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest
+import org.testcontainers.DockerClientFactory
 import reactor.test.StepVerifier
 import ru.somarov.auth_service.backend.db.entity.Privilege
 import ru.somarov.auth_service.backend.db.repository.PrivilegeRepo
@@ -15,6 +17,9 @@ class DatabaseTest {
 
     companion object {
         init {
+            if ( DockerClientFactory.instance() != null) {
+                println("kek")
+            }
             // Postgres JDBC driver uses JUL; disable it to avoid annoying,
             // irrelevant, stderr logs during connection testing
             LogManager.getLogManager().getLogger("").level = Level.OFF
