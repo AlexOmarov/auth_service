98743c0c898a8360215e8107ecfc45ed46830ccc
AlexOmarov <shtil.a@yandex.ru>
2020-11-16 15:26:08 +0700

    Made authentication

--- src/main/kotlin/ru/somarov/auth_service/backend/controller/LoginController.kt
+++ src/main/kotlin/ru/somarov/auth_service/backend/controller/LoginController.kt
@@ -1,14 +1,10 @@
 package ru.somarov.auth_service.backend.controller
 
-import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
-import ru.somarov.auth_service.api.LoginRequest
 import ru.somarov.auth_service.backend.config.log.Loggable
 
 
