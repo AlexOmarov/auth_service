75f3a345e4f17169d7520825d15a1abfd44492b7
Alex Omarov <shtil.a@yandex.ru>
2023-10-23 14:25:37 +0700

    SBS-1 Init service

--- auth_app/src/main/kotlin/ru/berte/auth/infrastructure/config/SecurityConfig.kt
+++ auth_app/src/main/kotlin/ru/berte/auth/infrastructure/config/SecurityConfig.kt
@@ -1,98 +1,136 @@
 package ru.berte.auth.infrastructure.config
 
+import com.nimbusds.jose.jwk.JWKSet
+import com.nimbusds.jose.jwk.RSAKey
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet
+import com.nimbusds.jose.jwk.source.JWKSource
+import com.nimbusds.jose.proc.SecurityContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
-import org.springframework.http.HttpStatus
-import org.springframework.security.authentication.ReactiveAuthenticationManager
-import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder
-import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.core.Authentication
+import org.springframework.core.annotation.Order
+import org.springframework.http.MediaType
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod
+import org.springframework.security.oauth2.core.oidc.OidcScopes
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
-import org.springframework.security.web.server.SecurityWebFilterChain
-import org.springframework.security.web.server.WebFilterExchange
-import org.springframework.security.web.server.authentication.AuthenticationWebFilter
-import org.springframework.security.web.server.authentication.ServerFormLoginAuthenticationConverter
-import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository
-import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher
-import reactor.core.publisher.Mono
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher
+import ru.berte.auth.infrastructure.db.repository.AccountRepo
+import ru.berte.auth.infrastructure.db.repository.PrivilegeRepo
+import ru.berte.auth.infrastructure.db.repository.RoleRepo
 import ru.berte.auth.infrastructure.security.UserDetailsServiceImpl
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.util.*
 
 
 @Configuration
 class SecurityConfig {
-
     @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return BCryptPasswordEncoder()
-    }
-
-    @Bean
-    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+    @Order(1)
+    fun authorizationServerSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http)
-        return http
-            .addFilterAt(authenticationWebFilter(), SecurityWebFiltersOrder.AUTHENTICATION)
-            .csrf().disable()
-            .httpBasic().disable()
-            .formLogin().disable()
-            .authorizeExchange()
-            // Path matchers relates to authorization filter
-            // Because I decided to authenticate user with custom filter on login,
-            // if we will call post login without form data it will be 401 code
-            // On all other endpoints will be only authorization filter, which will return 401/403 code
-            .pathMatchers("/login").permitAll()
-            .pathMatchers("/actuator/*").permitAll()
-            .pathMatchers("/**").authenticated()
-            .and().logout().logoutUrl("/perform_logout")
-            .logoutSuccessHandler { webFilterExchange: WebFilterExchange, _: Authentication ->
-                run {
-                    webFilterExchange.exchange.response.statusCode = HttpStatus.OK
-                }
-                Mono.empty<Void>()
-            }
-            .and().build()
+        // Enable OpenID Connect 1.0
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer::class.java).oidc(Customizer.withDefaults())
+        // Redirect to the login page when not authenticated from the authorization endpoint
+        http.exceptionHandling { exceptions ->
+            exceptions.defaultAuthenticationEntryPointFor(
+                LoginUrlAuthenticationEntryPoint("/login"),
+                MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+            )
+        }
+        // Accept access tokens for User Info and/or Client Registration
+        http.oauth2ResourceServer { resourceServer: OAuth2ResourceServerConfigurer<HttpSecurity?> ->
+            resourceServer.jwt(Customizer.withDefaults())
+        }
+        return http.build()
     }
 
     @Bean
-    fun authenticationWebFilter(): AuthenticationWebFilter {
-        val authenticationWebFilter = AuthenticationWebFilter(authenticationManager())
-        authenticationWebFilter.setSecurityContextRepository(webSessionContextRepository())
-        authenticationWebFilter.setAuthenticationConverter(authenticationConverter())
-        authenticationWebFilter.setRequiresAuthenticationMatcher(requiresAuthenticationMatcher())
-        return authenticationWebFilter
+    @Order(2)
+    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests { it.anyRequest().authenticated() }
+        // Form login handles the redirect to the login page from the
+        // authorization server filter chain
+        http.formLogin(Customizer.withDefaults())
+        return http.build()
     }
 
     @Bean
-    fun authenticationManager(): ReactiveAuthenticationManager {
-        val udrram = UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService())
-        udrram.setPasswordEncoder(passwordEncoder())
-        return udrram
+    fun userDetailsService(
+        accountRepo: AccountRepo,
+        roleRepo: RoleRepo,
+        privilegeRepo: PrivilegeRepo
+    ): ReactiveUserDetailsService {
+        return UserDetailsServiceImpl(
+            accountRepo = accountRepo,
+            roleRepo = roleRepo,
+            privilegeRepo = privilegeRepo
+        )
     }
 
     @Bean
-    fun authenticationConverter(): ServerFormLoginAuthenticationConverter {
-        return ServerFormLoginAuthenticationConverter()
+    fun registeredClientRepository(): RegisteredClientRepository {
+        val oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+            .clientId("oidc-client")
+            .clientSecret("{noop}secret")
+            .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+            .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+            .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
+            .postLogoutRedirectUri("http://127.0.0.1:8080/")
+            .scope(OidcScopes.OPENID)
+            .scope(OidcScopes.PROFILE)
+            .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+            .build()
+        return InMemoryRegisteredClientRepository(oidcClient)
     }
 
     @Bean
-    fun userDetailsService(): ReactiveUserDetailsService {
-        return UserDetailsServiceImpl()
+    fun jwkSource(): JWKSource<SecurityContext> {
+        val keyPair = generateRsaKey()
+        val publicKey = keyPair.public as RSAPublicKey
+        val privateKey = keyPair.private as RSAPrivateKey
+        val rsaKey: RSAKey = RSAKey.Builder(publicKey)
+            .privateKey(privateKey)
+            .keyID(UUID.randomUUID().toString())
+            .build()
+        val jwkSet = JWKSet(rsaKey)
+        return ImmutableJWKSet(jwkSet)
+    }
+
+    private fun generateRsaKey(): KeyPair {
+        val keyPair = try {
+            val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
+            keyPairGenerator.initialize(2048)
+            keyPairGenerator.generateKeyPair()
+        } catch (ex: Exception) {
+            throw IllegalStateException(ex)
+        }
+        return keyPair
     }
 
     @Bean
-    fun webSessionContextRepository(): WebSessionServerSecurityContextRepository {
-        return WebSessionServerSecurityContextRepository()
+    fun jwtDecoder(jwkSource: JWKSource<SecurityContext>): JwtDecoder {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource)
     }
 
     @Bean
-    fun requiresAuthenticationMatcher(): ServerWebExchangeMatcher {
-        return PathPatternParserServerWebExchangeMatcher("/login", HttpMethod.POST)
+    fun authorizationServerSettings(): AuthorizationServerSettings {
+        return AuthorizationServerSettings.builder().build()
     }
-
-
 }
